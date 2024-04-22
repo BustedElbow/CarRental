45a1624644cfe2d2962e26498272a685e5497bd6
bustedelbow <velozity2@gmail.com>
2024-04-22 21:02:52 +0800

    Adjusting the position of UserLoginFrame

--- login.py
+++ login.py
@@ -1,35 +1,53 @@
 import customtkinter 
 
 class UserLoginFrame(customtkinter.CTkFrame):
-  def __init__(self, master, **kwargs):
-    super().__init__(master, **kwargs)
-    
-    windowBgClr = self.cget('background')
-
+  def __init__(self, master, fg_color, bg_color):
+    super().__init__(master, fg_color=fg_color, bg_color=bg_color)
+   
     self.userLabel = customtkinter.CTkLabel(
       self, 
       text='Username', 
-      bg_color=windowBgClr, 
-      text_color='black'
+      bg_color=fg_color,
+      text_color='white',
+      anchor='w'
       )
+    self.userLabel.pack(pady=(10, 0), padx=(12, 0) , fill='x')
 
     self.userEntry = customtkinter.CTkEntry(
       self, 
-      bg_color=windowBgClr, 
-      fg_color=windowBgClr
+      bg_color=fg_color,
+      fg_color='white',
+      text_color='black'
       )
-    self.userEntry.pack()
+    self.userEntry.pack(padx=10)
+
+    self.passLabel = customtkinter.CTkLabel(
+      self,
+      text='Password',
+      bg_color=fg_color,
+      text_color='white',
+      anchor='w'
+    )
+    self.passLabel.pack(pady=(10, 0), padx=(12,0), fill='x')
+
+    self.passEntry = customtkinter.CTkEntry(
+      self,
+      bg_color=fg_color,
+      fg_color='white',
+      text_color='black',
+      show='*'
+    )
+    self.passEntry.pack()
 
     self.loginBtn = customtkinter.CTkButton(
       self, 
       text='login', 
-      corner_radius=16,
+      corner_radius=8,
       fg_color='#65AFFF', 
-      bg_color=windowBgClr)
-    self.loginBtn.pack(pady=20)
+      bg_color=fg_color,
+    )
+    self.loginBtn.pack(pady=(10, 20), padx=10)
 
-    self.mainloop()
-    self.userLabel.pack()
 
 
 class LoginWindow(customtkinter.CTk):
@@ -39,6 +57,15 @@ class LoginWindow(customtkinter.CTk):
     self.geometry('700x500')
     self._set_appearance_mode('light')
 
+    windowBgClr = self.cget('background')
+    self.Uframe = UserLoginFrame(
+      master=self, 
+      bg_color=windowBgClr, 
+      fg_color='#1B2845',
+      )
+    self.Uframe.place(relx=1, rely=0.5, anchor='e', x=1)
+
+    self.mainloop()
 
 
 LoginWindow()
