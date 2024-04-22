14bd6c4751ad98838bd0b6d6617694740a0235f3
bustedelbow <velozity2@gmail.com>
2024-04-22 20:09:28 +0800

    Disabled login component

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 import customtkinter as tk
+from login import LoginWindow
 
-root = tk.CTk()
-root.title("Login Page")
-root.geometry('700x500')
 
-tk.set_appearance_mode('light')
-
-btn = tk.CTkButton(master=root, text='Login', corner_radius=8, fg_color='blue', hover_color='red')
-
-btn.place(relx=0.5, rely=0.6, anchor='center')
-
-root.mainloop()
+# login = LoginWindow()
