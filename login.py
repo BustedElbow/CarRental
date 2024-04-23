4db5b87252fc5f51f903f5362244d42e09cd3238
bustedelbow <velozity2@gmail.com>
2024-04-23 18:31:26 +0800

    Overhauled the entire GUI

--- login.py
+++ login.py
@@ -1,71 +1,40 @@
 import customtkinter 
 
-class UserLoginFrame(customtkinter.CTkFrame):
-  def __init__(self, master, fg_color, bg_color):
-    super().__init__(master, fg_color=fg_color, bg_color=bg_color)
-   
-    self.userLabel = customtkinter.CTkLabel(
-      self, 
-      text='Username', 
-      bg_color=fg_color,
-      text_color='white',
-      anchor='w'
-      )
-    self.userLabel.pack(pady=(10, 0), padx=(12, 0) , fill='x')
+customtkinter.set_appearance_mode('light')
 
-    self.userEntry = customtkinter.CTkEntry(
-      self, 
-      bg_color=fg_color,
-      fg_color='white',
-      text_color='black'
-      )
-    self.userEntry.pack(padx=10)
+app=customtkinter.CTk()
+app.geometry('800x450')
+app.resizable(False, False)
+app.title('login')
 
-    self.passLabel = customtkinter.CTkLabel(
-      self,
-      text='Password',
-      bg_color=fg_color,
-      text_color='white',
-      anchor='w'
-    )
-    self.passLabel.pack(pady=(10, 0), padx=(12,0), fill='x')
+mainClr = '#1F487E'
+actClr = '#E94C55'
 
-    self.passEntry = customtkinter.CTkEntry(
-      self,
-      bg_color=fg_color,
-      fg_color='white',
-      text_color='black',
-      show='*'
-    )
-    self.passEntry.pack()
+loginFrame=customtkinter.CTkFrame(master=app, width=320, height=340, fg_color=mainClr, corner_radius=16)
+loginFrame.place(relx=1, rely=0.5, anchor='e', x=-30)
 
-    self.loginBtn = customtkinter.CTkButton(
-      self, 
-      text='login', 
-      corner_radius=8,
-      fg_color='#65AFFF', 
-      bg_color=fg_color,
-    )
-    self.loginBtn.pack(pady=(10, 20), padx=10)
+loginHeading=customtkinter.CTkLabel(master=loginFrame, text='Login into your Account', font=('Helvetica', 16, 'bold'), text_color='white')
+loginHeading.place(x=70, y=45)
+
+userLabel=customtkinter.CTkLabel(master=loginFrame, text='Username', font=('Helvetica', 12, 'bold'), text_color='white')
+userLabel.place(x=45,y=90)
+
+userEntry=customtkinter.CTkEntry(master=loginFrame, width=240, border_color=mainClr, text_color='black', corner_radius=8, height=35)
+userEntry.place(x=40, y=115)
+
+passLabel=customtkinter.CTkLabel(master=loginFrame, text='Password', font=('Helvetica', 12, 'bold'), text_color='white')
+passLabel.place(x=45, y=165)
+
+passEntry=customtkinter.CTkEntry(master=loginFrame, width=240, border_color=mainClr, text_color='black', corner_radius=8, height=35, show='*')
+passEntry.place(x=40, y=190)
+
+loginBtn=customtkinter.CTkButton(master=loginFrame, text='Login', font=('Helvetica', 12, 'bold'), text_color='black', fg_color='white', width=110)
+loginBtn.place(x=43, y=250)
+
+signBtn=customtkinter.CTkButton(master=loginFrame, text='Sign Up', font=('Helvetica', 12, 'bold'), fg_color=actClr, text_color='white', width=110)
+signBtn.place(x=168, y=250)
 
 
-
-class LoginWindow(customtkinter.CTk):
-  def __init__(self):
-    super().__init__()
-    self.title('Login')
-    self.geometry('700x500')
-    self._set_appearance_mode('light')
-
-    windowBgClr = self.cget('background')
-    self.Uframe = UserLoginFrame(
-      master=self, 
-      bg_color=windowBgClr, 
-      fg_color='#1B2845',
-      )
-    self.Uframe.place(relx=1, rely=0.5, anchor='e', x=-30)
-
-    self.mainloop()
+app.mainloop()
 
 
-LoginWindow()
