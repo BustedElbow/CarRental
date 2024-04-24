31359a07e39fbc783c20f564809a258850abfe5a
bustedelbow <velozity2@gmail.com>
2024-04-24 19:42:57 +0800

    Basic prototype for class. cards

--- main.py
+++ main.py
@@ -7,18 +7,34 @@ class mainWindow(customtkinter.CTk):
     screen_height = self.winfo_screenheight()
     x = (screen_width - 1600) // 2  
     y = (screen_height - 900) // 2
+    self._set_appearance_mode('dark')
+
+    mainClr = '#4B9AF6'
+    actClr = '#1C3076'
 
     self.geometry(f'1600x900+{x}+{y}')
    
     self.title('RMJ Car Rentals')
 
-    self.frame=customtkinter.CTkFrame(self, width=200, height=200, fg_color='blue', corner_radius=16) 
-    self.frame.place(relx=0.5, rely=0.5, anchor='center')
+    self.topFrame=customtkinter.CTkFrame(self, width=150, height=900, fg_color=mainClr, corner_radius=0) 
+    self.topFrame.place(relx=0, rely=0.5, anchor='w')
 
-    self._set_appearance_mode('dark')
-    self.label=customtkinter.CTkLabel(self.frame, text='FUCK')
-    self.label.place(x=10, y=10)
+    self.classlabel=customtkinter.CTkLabel(self, text='Vehicle Classification', font=('Helvetica', 32, 'bold'), text_color=mainClr)
+    self.classlabel.place(x=300, y=20)
+
+    self.famFrame=customtkinter.CTkFrame(self, width=350, height=600)
+    self.famFrame.place(x=300, y=160)
+
+    self.vacFrame=customtkinter.CTkFrame(self, width=350, height=600)
+    self.vacFrame.place(x=690, y=160)
+
+    # self.movFrame=customtkinter.CTkFrame(self, width=350, height=600)
+
+
+
+
 
     self.mainloop()
 
 
+mainWindow()
