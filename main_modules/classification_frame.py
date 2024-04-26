2d6120de8ab39a4cd8d5682eec67621e465ee3c5
bustedelbow <velozity2@gmail.com>
2024-04-26 19:51:03 +0800

    Changed image placeholder into real image

--- main_modules/classification_frame.py
+++ main_modules/classification_frame.py
@@ -1,4 +1,6 @@
 import customtkinter
+import os
+from PIL import Image
 
 class classificationFrame(customtkinter.CTkFrame):
   def showFamPage(self):
@@ -16,9 +18,15 @@ class classificationFrame(customtkinter.CTkFrame):
     self.famCardFrame=customtkinter.CTkFrame(self, width=350, height=600, fg_color='white', corner_radius=16)
     self.famCardFrame.place(x=130, y=160)
 
-    self.famImgCon=customtkinter.CTkFrame(self.famCardFrame, width=280, height=280, fg_color='gray', corner_radius=16)
-    self.famImgCon.place(x=35, y=30)
+    ## Family Image Path
+    self.famMainDir=os.path.dirname(os.path.realpath(__file__))
+    self.famFolderPath=os.path.join(self.famMainDir, '../images/family')
+    self.famImagePath=os.path.join(self.famFolderPath, 'toyotaInnova.jpg')
 
+    self.famImg=customtkinter.CTkImage(light_image=Image.open(self.famImagePath), size=(320,170))
+    self.famImgLabel=customtkinter.CTkLabel(self.famCardFrame, image=self.famImg, text='')
+    self.famImgLabel.place(x=20, y=150)
+  
     self.famLabel=customtkinter.CTkLabel(self.famCardFrame, text='Family', font=('Helvetica', 32, 'bold'), text_color='black')
     self.famLabel.place(x=125, y=325)
 
@@ -26,28 +34,40 @@ class classificationFrame(customtkinter.CTkFrame):
     self.famBtn.place(x=75, y=520)
 
 
-    # # Vacation Frame
-    self.vacFrame=customtkinter.CTkFrame(self, width=350, height=600, fg_color='white', corner_radius=16)
-    self.vacFrame.place(x=565, y=160)
+    # Vacation Frame
+    self.vacCardFrame=customtkinter.CTkFrame(self, width=350, height=600, fg_color='white', corner_radius=16)
+    self.vacCardFrame.place(x=565, y=160)
 
-    self.vacImgCon=customtkinter.CTkFrame(self.vacFrame, width=280, height=280, fg_color='gray', corner_radius=16)
-    self.vacImgCon.place(x=35, y=30)
+    ## Vacation Image path
+    self.vacMainDir=os.path.dirname(os.path.realpath(__file__))
+    self.vacFolderPath=os.path.join(self.vacMainDir, '../images/vacation')
+    self.vacImagePath=os.path.join(self.vacFolderPath, 'toyotaHiace.jpg')
 
-    self.vacLabel=customtkinter.CTkLabel(self.vacFrame, text='Vacation', font=('Helvetica', 32, 'bold'), text_color='black')
+    self.vacImg=customtkinter.CTkImage(light_image=Image.open(self.vacImagePath), size=(320,220))
+    self.vacImgLabel=customtkinter.CTkLabel(self.vacCardFrame, image=self.vacImg, text='')
+    self.vacImgLabel.place(x=20, y=90)
+
+    self.vacLabel=customtkinter.CTkLabel(self.vacCardFrame, text='Vacation', font=('Helvetica', 32, 'bold'), text_color='black')
     self.vacLabel.place(x=110, y=325)
 
-    self.vacBtn=customtkinter.CTkButton(self.vacFrame, text='View', width=200, height=50, fg_color=mainClr, font=('Helvetica', 24, 'bold'), corner_radius=16, text_color='black')
+    self.vacBtn=customtkinter.CTkButton(self.vacCardFrame, text='View', width=200, height=50, fg_color=mainClr, font=('Helvetica', 24, 'bold'), corner_radius=16, text_color='black')
     self.vacBtn.place(x=75, y=520)
 
-    # # Mover Frame
-    self.movFrame=customtkinter.CTkFrame(self, width=350, height=600, fg_color='white', corner_radius=16)
-    self.movFrame.place(x=1000, y=160)
+    # Mover Frame
+    self.movCardFrame=customtkinter.CTkFrame(self, width=350, height=600, fg_color='white', corner_radius=16)
+    self.movCardFrame.place(x=1000, y=160)
 
-    self.movImgCon=customtkinter.CTkFrame(self.movFrame, width=280, height=280, fg_color='gray', corner_radius=16)
-    self.movImgCon.place(x=35, y=30)
+    ## Mover Image Path
+    self.movMainDir=os.path.dirname(os.path.realpath(__file__))
+    self.movFolderPath=os.path.join(self.movMainDir, '../images/mover')
+    self.movImagePath=os.path.join(self.movFolderPath, 'mazdaBongo.jpg')
 
-    self.movLabel=customtkinter.CTkLabel(self.movFrame, text='Mover', font=('Helvetica', 32, 'bold'), text_color='black')
+    self.movImg=customtkinter.CTkImage(light_image=Image.open(self.movImagePath), size=(320,200))
+    self.movImgLabel=customtkinter.CTkLabel(self.movCardFrame, image=self.movImg, text='')
+    self.movImgLabel.place(x=20, y=110)
+
+    self.movLabel=customtkinter.CTkLabel(self.movCardFrame, text='Mover', font=('Helvetica', 32, 'bold'), text_color='black')
     self.movLabel.place(x=125, y=325)
 
-    self.movBtn=customtkinter.CTkButton(self.movFrame, text='View', width=200, height=50, fg_color=mainClr, font=('Helvetica', 24, 'bold'), corner_radius=16, text_color='black')
+    self.movBtn=customtkinter.CTkButton(self.movCardFrame, text='View', width=200, height=50, fg_color=mainClr, font=('Helvetica', 24, 'bold'), corner_radius=16, text_color='black')
     self.movBtn.place(x=75, y=520)
