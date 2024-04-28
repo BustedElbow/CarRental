b221d4f3bc486e2f934b1958e3e00a67aa035595
bustedelbow <velozity2@gmail.com>
2024-04-28 14:55:08 +0800

    Added scroll function

--- main_modules/family_frame.py
+++ main_modules/family_frame.py
@@ -1,5 +1,5 @@
 import customtkinter
-
+from tkinter import ttk
 
 class familyFrame(customtkinter.CTkFrame):
   def showPreviousFrame(self):
@@ -15,3 +15,28 @@ class familyFrame(customtkinter.CTkFrame):
 
     self.backBtn=customtkinter.CTkButton(self, text='Back', font=('Helvetica', 16, 'bold'), command=self.showPreviousFrame, width=50, height=50, corner_radius=50 // 2)
     self.backBtn.place(x=20, y=20)
+
+    self.canvas=customtkinter.CTkCanvas(self, bg=frameClr, width=1450, height=815, highlightthickness=0, scrollregion=(0, 0, 1000, 2000))
+    self.canvas.place(x=1, y=85)
+
+    self.scrollbar=ttk.Scrollbar(self, orient='vertical', command=self.canvas.yview)
+    self.scrollbar.place(relx=1, rely=0, relheight=1, anchor='ne')
+
+    self.canvas.bind('<MouseWheel>', lambda event: self.canvas.yview_scroll(int(event.delta / -60), 'units'))
+
+    self.canvas.config(yscrollcommand=self.scrollbar.set)
+
+    
+    self.carFrame=customtkinter.CTkFrame(self.canvas, fg_color='white', width=350, height=600, corner_radius=16)
+    self.canvas.create_window((130, 75), window=self.carFrame, anchor='nw')
+
+    self.carFrame2=customtkinter.CTkFrame(self.canvas, fg_color='white', width=350, height=600, corner_radius=16)
+    self.canvas.create_window((565, 75), window=self.carFrame2, anchor='nw')
+
+    self.carFrame3=customtkinter.CTkFrame(self.canvas, fg_color='white', width=350, height=600, corner_radius=16)
+    self.canvas.create_window((1000, 75), window=self.carFrame3, anchor='nw')
+
+    self.carFrame4=customtkinter.CTkFrame(self.canvas, fg_color='white', width=350, height=600, corner_radius=16)
+    self.canvas.create_window((130, 750), window=self.carFrame4, anchor='nw')
+
+    
