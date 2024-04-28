93fd925aacf358f19fe3b2d09b6c4b5890f3c88a
bustedelbow <velozity2@gmail.com>
2024-04-28 15:29:23 +0800

    Added placeholder cards for vehicles

--- main_modules/vacation_frame.py
+++ main_modules/vacation_frame.py
@@ -1,4 +1,5 @@
 import customtkinter
+from tkinter import ttk
 
 class vacationFrame(customtkinter.CTkFrame):
   def showPreviousFrame(self):
@@ -15,14 +16,28 @@ class vacationFrame(customtkinter.CTkFrame):
     self.backBtn=customtkinter.CTkButton(self, text='Back', font=('Helvetica', 16, 'bold'), command=self.showPreviousFrame, width=50, height=50, corner_radius=50 // 2)
     self.backBtn.place(x=20, y=20)
 
-    self.canvas=customtkinter.CTkCanvas(self, bg=frameClr, width=1450, height=815, highlightthickness=0, scrollregion=(0, 0, 0, 1500))
+    self.canvas=customtkinter.CTkCanvas(self, bg=frameClr, width=1450, height=815, highlightthickness=0, scrollregion=(0, 0, 1000, 1420))
     self.canvas.place(x=1, y=85)
 
+    self.scrollbar=ttk.Scrollbar(self, orient='vertical', command=self.canvas.yview)
+    self.scrollbar.place(relx=1, rely=0, relheight=1, anchor='ne')
+
+    self.canvas.bind('<MouseWheel>', lambda event: self.canvas.yview_scroll(int(event.delta / -60), 'units'))
+    self.bind('<MouseWheel>', lambda event: self.canvas.yview_scroll(int(event.delta / -60), 'units'))
+
+    self.canvas.config(yscrollcommand=self.scrollbar.set)
+
     self.carFrame=customtkinter.CTkFrame(self.canvas, fg_color='white', width=350, height=600, corner_radius=16)
-    self.carFrame.place(x=130, y=75)
+    self.canvas.create_window((130, 75), window=self.carFrame, anchor='nw')
 
     self.carFrame2=customtkinter.CTkFrame(self.canvas, fg_color='white', width=350, height=600, corner_radius=16)
-    self.carFrame2.place(x=565, y=75)
+    self.canvas.create_window((565, 75), window=self.carFrame2, anchor='nw')
 
     self.carFrame3=customtkinter.CTkFrame(self.canvas, fg_color='white', width=350, height=600, corner_radius=16)
-    self.carFrame3.place(x=1000, y=75)
+    self.canvas.create_window((1000, 75), window=self.carFrame3, anchor='nw')
+
+    self.carFrame4=customtkinter.CTkFrame(self.canvas, fg_color='white', width=350, height=600, corner_radius=16)
+    self.canvas.create_window((130, 750), window=self.carFrame4, anchor='nw')
+
+    self.carFrame5=customtkinter.CTkFrame(self.canvas, fg_color='white', width=350, height=600, corner_radius=16)
+    self.canvas.create_window((565, 750), window=self.carFrame5, anchor='nw')
