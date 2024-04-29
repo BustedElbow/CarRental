e7b12e2bab0007e9df8c2f642968ab00c807e149
bustedelbow <velozity2@gmail.com>
2024-04-29 18:48:08 +0800

    Set resizable to false & fixed initialization errors

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import customtkinter
+from main_modules.classification_frame import classificationFrame
 from main_modules.side_frame import sideFrame
 from main_modules.family_frame import familyFrame
-from main_modules.classification_frame import classificationFrame
-
+from main_modules.vacation_frame import vacationFrame
+from main_modules.mover_frame import moverFrame
 
 class mainWindow(customtkinter.CTk):
-
-  def showFamPage(self):
-    self.famFrame.tkraise()
-
   def __init__(self):
     super().__init__()
     screen_width = self.winfo_screenwidth()
@@ -16,6 +13,7 @@ class mainWindow(customtkinter.CTk):
     x = (screen_width - 1600) // 2  
     y = (screen_height - 900) // 2
     self._set_appearance_mode('dark')
+    self.resizable(False, False)
 
     mainClr = '#4B9AF6'
     actClr = '#1C3076'
@@ -29,14 +27,22 @@ class mainWindow(customtkinter.CTk):
     self.sideFrame=sideFrame(self, fg_color=mainClr)
     self.sideFrame.place(relx=0, rely=0.5, anchor='w')
 
-    self.famFrame=familyFrame(self, fg_color=frameClr, text_color=mainClr)
+    self.famFrame=familyFrame(self, frameClr=frameClr, mainClr=mainClr, previousFrame=None)
     self.famFrame.place(relx=1, rely=0.5, anchor='e')
 
-    self.clsfMainFrame=classificationFrame(self, mainClr=mainClr, frameClr=frameClr, family_frame=self.famFrame)
+    self.vacFrame=vacationFrame(self, frameClr=frameClr, mainClr=mainClr, previousFrame=None)
+    self.vacFrame.place(relx=1, rely=0.5, anchor='e')
+
+    self.movFrame=moverFrame(self, frameClr=frameClr, mainClr=mainClr, previousFrame=None)
+    self.movFrame.place(relx=1, rely=0.5, anchor='e')
+
+    self.clsfMainFrame=classificationFrame(self, mainClr=mainClr, frameClr=frameClr, familyFrame=self.famFrame, vacationFrame=self.vacFrame, moverFrame=self.movFrame)
     self.clsfMainFrame.place(relx=1, rely=0.5, anchor='e')
 
+    self.vacFrame.previousFrame=self.clsfMainFrame
+    self.famFrame.previousFrame=self.clsfMainFrame
+    self.movFrame.previousFrame=self.clsfMainFrame
 
     self.mainloop()
-
 
 mainWindow()
