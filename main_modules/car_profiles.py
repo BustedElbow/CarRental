b79b320ad8becd5f0c06030c7c35377a485c61f4
bustedelbow <velozity2@gmail.com>
2024-04-30 21:44:39 +0800

    Added car profiles file

---
+++ main_modules/car_profiles.py
@@ -0,0 +1,85 @@
+
+
+carsFamily = [
+    {
+      'id': 1,
+      'manufacturer': 'Toyota',
+      'model': 'Innova',
+      'fuel': 'Unleaded',
+      'seats': '5 seats',
+      'price': 3000,
+    },
+    {
+      'id': 2,
+      'manufacturer': 'Toyota',
+      'model': 'Vios',
+    },
+    {
+      'id': 3,
+      'manufacturer': 'Kia',
+      'model': 'Soluto',
+    },
+    {
+      'id': 4,
+      'manufacturer': 'Suzuki',
+      'model': 'Dzire',
+    },
+    {
+      'id': 5,
+      'manufacturer': 'Mitsubishi',
+      'model': 'Mirage',
+    },
+    {
+      'id': 6,
+      'manufacturer': 'Toyota',
+      'model': 'Avanza',
+    },
+    {
+      'id': 7,
+      'manufacturer': 'Toyota',
+      'model': 'Fortuner',
+    },
+    {
+      'id': 8,
+      'manufacturer': 'Toyota',
+      'model': 'Rush'
+    },
+    {
+      'id': 9,
+      'manufacturer': 'Toyota',
+      'model': 'Land Cruiser',
+    },
+    {
+      'id': 10,
+      'manufacturer': 'Mg',
+      'model': 'Five',
+    }
+]
+
+carsVacation = [
+  {
+    'id': 1,
+    'manufacturer': 'Toyota',
+    'model': 'Hiace'
+  },
+  {
+    'id': 2,
+    'manufacturer': 'Hyundai',
+    'model': 'Starex',
+  },
+  {
+    'id': 3,
+    'manufacturer': 'Nissan',
+    'model': 'Urvan',
+  },
+  {
+    'id': 4,
+    'manufacturer': 'Mitsubishi',
+    'model': 'L 300'
+  },
+  {
+    'id': 5,
+    'manufacturer': 'Traditional',
+    'model': 'Jeepney',
+  }
+]
