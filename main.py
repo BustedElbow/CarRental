288b2b905fba0aca523090630bffc6c5db2904ee
bustedelbow <velozity2@gmail.com>
2024-05-01 14:36:04 +0800

    Fixed initialization error

--- main.py
+++ main.py
@@ -17,46 +17,49 @@ class mainWindow(customtkinter.CTk):
     self._set_appearance_mode('dark')
     self.resizable(False, False)
 
-    mainClr = '#4B9AF6'
-    actClr = '#1C3076'
-    frameClr = '#242424'
+    main_color = '#4B9AF6'
+    sec_color = '#1C3076'
+    frame_color = '#242424'
 
     self.geometry(f'1600x900+{x}+{y}')
    
     self.title('RMJ Car Rentals')
 
 
-    self.sideFrame=sideFrame(self, fg_color=mainClr)
-    self.sideFrame.place(relx=0, rely=0.5, anchor='w')
+    self.sideFrame = sideFrame(self, fg_color = main_color)
 
-    self.famFrame=familyFrame(self, frameClr=frameClr, mainClr=mainClr, previousFrame=None, orderFrame=None, cars=carsFamily)
-    self.famFrame.place(relx=1, rely=0.5, anchor='e')
+    self.famFrame = familyFrame(self, frame_color = frame_color, main_color = main_color, previousFrame = None, orderFrame = None, cars = carsFamily)
 
-    self.vacFrame=vacationFrame(self, frameClr=frameClr, mainClr=mainClr, previousFrame=None, orderFrame=None, cars=carsVacation)
-    self.vacFrame.place(relx=1, rely=0.5, anchor='e')
+    self.vacFrame = vacationFrame(self, frame_color = frame_color, main_color = main_color, previousFrame = None, orderFrame = None, cars = carsVacation)
 
-    self.movFrame=moverFrame(self, frameClr=frameClr, mainClr=mainClr, previousFrame=None, orderFrame=None, cars=carsMover)
-    self.movFrame.place(relx=1, rely=0.5, anchor='e')
+    self.movFrame = moverFrame(self, frame_color = frame_color, main_color = main_color, previousFrame = None, orderFrame = None, cars = carsMover)
 
-    self.famOrderFrame=orderFrame(self, mainClr=mainClr, frameClr=frameClr, previousFrame=self.famFrame)
-    self.famOrderFrame.place(relx=1, rely=0.5, anchor='e')
+    self.famOrderFrame = orderFrame(self, main_color = main_color, frame_color = frame_color, prev_frame = self.famFrame)
 
-    self.vacOrderFrame=orderFrame(self, mainClr=mainClr, frameClr=frameClr, previousFrame=self.vacFrame)
-    self.vacOrderFrame.place(relx=1, rely=0.5, anchor='e')
+    self.vacOrderFrame = orderFrame(self, main_color = main_color, frame_color = frame_color, prev_frame = self.vacFrame)
 
-    self.movOrderFrame=orderFrame(self, mainClr=mainClr, frameClr=frameClr, previousFrame=self.movFrame)
-    self.movOrderFrame.place(relx=1, rely=0.5, anchor='e')
+    self.movOrderFrame = orderFrame(self, main_color = main_color, frame_color = frame_color, prev_frame = self.movFrame)
 
-    self.clsfMainFrame=classificationFrame(self, mainClr=mainClr, frameClr=frameClr, familyFrame=self.famFrame, vacationFrame=self.vacFrame, moverFrame=self.movFrame)
-    self.clsfMainFrame.place(relx=1, rely=0.5, anchor='e')
+    self.clsfMainFrame = classificationFrame(self, main_color = main_color, frame_color = frame_color, fam_frame = self.famFrame, vac_frame = self.vacFrame, mov_frame = self.movFrame)
 
-    self.vacFrame.previousFrame=self.clsfMainFrame
-    self.famFrame.previousFrame=self.clsfMainFrame
-    self.movFrame.previousFrame=self.clsfMainFrame
-    self.famFrame.orderFrame=self.famOrderFrame
-    self.vacFrame.orderFrame=self.vacOrderFrame
-    self.movFrame.orderFrame=self.movOrderFrame
 
+    self.famFrame.orderFrame = self.famOrderFrame
+    self.famFrame.previousFrame = self.clsfMainFrame
+
+    self.vacFrame.orderFrame = self.vacOrderFrame
+    self.vacFrame.previousFrame = self.clsfMainFrame
+    
+    self.movFrame.orderFrame = self.movOrderFrame
+    self.movFrame.previousFrame = self.clsfMainFrame
+
+    self.movOrderFrame.place(relx = 1, rely = 0.5, anchor = 'e')
+    self.vacOrderFrame.place(relx = 1, rely = 0.5, anchor = 'e')
+    self.famOrderFrame.place(relx = 1, rely = 0.5, anchor = 'e')
+    self.movFrame.place(relx = 1, rely = 0.5, anchor = 'e')
+    self.vacFrame.place(relx = 1, rely = 0.5, anchor = 'e')
+    self.famFrame.place(relx = 1, rely = 0.5, anchor = 'e')
+    self.sideFrame.place(relx = 0, rely = 0.5, anchor = 'w')
+    self.clsfMainFrame.place(relx = 1, rely = 0.5, anchor = 'e')
 
     self.mainloop()
 
