ba0905ec408dd821cf2178970d31aac092354e08
bustedelbow <velozity2@gmail.com>
2024-05-01 14:39:22 +0800

    Updated variable naming

--- main_modules/order_frame.py
+++ main_modules/order_frame.py
@@ -1,28 +1,25 @@
 import customtkinter
 
-def rentCar(carData):
-  return carData
-
 class orderFrame(customtkinter.CTkFrame):
-  def updateCarModel(self, model, manufacturer):
-    self.cartest.configure(text=f'Selected Car: {manufacturer} {model}')
+  def __init__(self, master, frame_color, main_color, prev_frame):
+    super().__init__(master, width = 1450, height = 900, fg_color = frame_color)
 
-  def showPreviousFrame(self):
+    self.previousFrame = prev_frame
+
+    self.main_heading = customtkinter.CTkLabel(self, text = 'Ordering Information', font = ('Helvetica', 36, 'bold'), text_color = main_color)
+    self.main_heading.place(x = 130, y = 20)
+
+    self.back_btn = customtkinter.CTkButton(self, text = 'Back', font = ('Helvetica', 16, 'bold'), command = self.show_prev_frame, width = 50, height = 50, corner_radius = 50 // 2, fg_color = main_color)
+    self.back_btn.place(x = 20, y = 20)
+
+    self.main_frame = customtkinter.CTkFrame(self, width = 500, height = 500, corner_radius = 16, fg_color = 'white')
+    self.main_frame.place(x = 200, y = 200)
+
+    self.car_test = customtkinter.CTkLabel(self.main_frame, text = '', font = ('Helvetica', 16, 'bold'), text_color = 'black')
+    self.car_test.place(x = 50, y = 50)
+
+  def update_car_model(self, model, manufacturer):
+    self.car_test.configure(text = f'Selected Car: {manufacturer} {model}')
+
+  def show_prev_frame(self):
     self.previousFrame.tkraise()
-
-  def __init__(self, master, frameClr, mainClr, previousFrame):
-    super().__init__(master, width=1450, height=900, fg_color=frameClr)
-
-    self.previousFrame=previousFrame
-
-    self.mainHeading=customtkinter.CTkLabel(self, text='Ordering Information', font=('Helvetica', 36, 'bold'), text_color=mainClr)
-    self.mainHeading.place(x=130, y=20)
-
-    self.backBtn=customtkinter.CTkButton(self, text='Back', font=('Helvetica', 16, 'bold'), command=self.showPreviousFrame, width=50, height=50, corner_radius=50 // 2, fg_color=mainClr)
-    self.backBtn.place(x=20, y=20)
-
-    self.mainFrame=customtkinter.CTkFrame(self, width=500, height=500, corner_radius=16, fg_color='white')
-    self.mainFrame.place(x=200, y=200)
-
-    self.cartest=customtkinter.CTkLabel(self.mainFrame, text='', font=('Helvetica', 16, 'bold'), text_color='black')
-    self.cartest.place(x=50, y=50)
