11b58c7a749b54dd4efdb90edd4b12f9c2e9db9a
bustedelbow <velozity2@gmail.com>
2024-05-03 21:25:04 +0800

    Fixed about button placement

--- main_modules/side_frame.py
+++ main_modules/side_frame.py
@@ -3,3 +3,6 @@ import customtkinter
 class sideFrame(customtkinter.CTkFrame):
   def __init__(self, master, fg_color):
     super().__init__(master, width=150, height=900, fg_color=fg_color, corner_radius=0)
+
+    self.about_btn = customtkinter.CTkButton(self, text = 'About', corner_radius = 16, width=110)
+    self.about_btn.place(x = 20, y = 850)
